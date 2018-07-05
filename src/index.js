885138926362e3130a3596a3729dbb1f1bba68f8
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-05 21:52:39 +0200

    Some test animations added

--- src/index.js
+++ src/index.js
@@ -3,15 +3,17 @@ import './scripts/dropdown';
 import './scripts/datetimeselector'
 import './scss/style.scss';
 
-$(document).ready(function(){
+$(document).ready(function () {
     //hamburger
-    $('.menu-trigger').click(function(){
+    $('.menu-trigger').click(function () {
         $('header ul').slideToggle(1000);
     });
-    $(window).resize(function(){
-        if ($(window).width() > 780 ) {
+    $(window).resize(function () {
+        if ($(window).width() > 780) {
             $('header ul').removeAttr('style');
         }
     });
+    $('.time-table').delay(600).animate({'opacity':'1'},1000);
+    $('.employee-card-box').delay(1000).animate({'opacity':'1'},1000);
+    $('footer').delay(1400).animate({'opacity':'1'},1000);
 });
-
