7f041b590340f56d1f38b7b1781ad63864302587
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-05 16:42:36 +0200

    Fixed datetimeselector.  Working on header

--- src/index.js
+++ src/index.js
@@ -4,8 +4,14 @@ import './scripts/datetimeselector'
 import './scss/style.scss';
 
 $(document).ready(function(){
-    $(function(){
-        $('.selectpicker').selectpicker();
+    //hamburger
+    $('.menu-trigger').click(function(){
+        $('header ul').slideToggle(1000);
+    });
+    $(window).resize(function(){
+        if ($(window).width() > 780 ) {
+            $('header ul').removeAttr('style');
+        }
     });
 });
 
