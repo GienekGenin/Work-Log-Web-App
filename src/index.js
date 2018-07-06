9ac2f0e2dd37af4c9d3fa1d7ac29b1db49b3db03
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-06 13:51:27 +0200

    timeselector styling

--- src/index.js
+++ src/index.js
@@ -5,23 +5,41 @@ import './scripts/hamburger';
 import './scss/style.scss';
 
 $(document).ready(function () {
+    getDate();
     // animations
     $('.time-table').delay(600).animate({'opacity': '1'}, 1000);
     $('.employee-card-box').delay(1000).animate({'opacity': '1'}, 1000);
     $('footer').delay(1400).animate({'opacity': '1'}, 1000);
 
-    // operations with datetime
-    // let a = document.getElementsByClassName('form-control')[0];
-    // function b(elem) {
-    //     setInterval(function () {
-    //         console.log(elem.value);
-    //     }, 1000);
-    // }
-    // b(a);
-    let datetime = $('.form-group');
-    let first_date = $('.form-control')[0];
-    let second_date = $('.form-control')[1];
-    // $("#datetimepicker1").on("change.datetimepicker", function (e) {
-    //     console.log('changed');
-    // });
+    // clock on the page
+    setInterval(function () {
+        getTime();
+    },1000);
+    //date on the page
+    setInterval(function () {
+        getDate();
+    },60000);
 });
+
+//get current time hh:mm:ss
+function getTime() {
+    let dt = new Date();
+    let time = dt.getHours() + ":" + dt.getMinutes() + ":" + dt.getSeconds();
+    $('.hms').text(time);
+}
+
+//get current date mm/dd/yyyy
+function getDate() {
+    let today = new Date();
+    let dd = today.getDate();
+    let mm = today.getMonth()+1; //January is 0!
+    let yyyy = today.getFullYear();
+    if(dd<10) {
+        dd = '0'+dd
+    }
+    if(mm<10) {
+        mm = '0'+mm
+    }
+    today = mm + '/' + dd + '/' + yyyy;
+    $('.current-date').text(`Today is: ${today}`);
+}
