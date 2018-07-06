abb7938abea4fa11dcdc427fb4b0d9194a82d875
Gennadii Genin <genin.gennadiigendos2010@yandex.ru>
2018-07-06 10:52:26 +0200

    managed to get date

--- src/index.js
+++ src/index.js
@@ -1,19 +1,27 @@
 import $ from 'jquery';
 import './scripts/dropdown';
-import './scripts/datetimeselector'
+// import './scripts/datetimeselector';
+import './scripts/hamburger';
 import './scss/style.scss';
 
 $(document).ready(function () {
-    //hamburger
-    // $('.menu-trigger').click(function () {
-    //     $('header ul').slideToggle(1000);
+    // animations
+    $('.time-table').delay(600).animate({'opacity': '1'}, 1000);
+    $('.employee-card-box').delay(1000).animate({'opacity': '1'}, 1000);
+    $('footer').delay(1400).animate({'opacity': '1'}, 1000);
+
+    // operations with datetime
+    // let a = document.getElementsByClassName('form-control')[0];
+    // function b(elem) {
+    //     setInterval(function () {
+    //         console.log(elem.value);
+    //     }, 1000);
+    // }
+    // b(a);
+    let datetime = $('.form-group');
+    let first_date = $('.form-control')[0];
+    let second_date = $('.form-control')[1];
+    // $("#datetimepicker1").on("change.datetimepicker", function (e) {
+    //     console.log('changed');
     // });
-    // $(window).resize(function () {
-    //     if ($(window).width() > 780) {
-    //         $('header ul').removeAttr('style');
-    //     }
-    // });
-    $('.time-table').delay(600).animate({'opacity':'1'},1000);
-    $('.employee-card-box').delay(1000).animate({'opacity':'1'},1000);
-    $('footer').delay(1400).animate({'opacity':'1'},1000);
 });
